33b3264c731953192f711d30422f0ec00681d2c6
Nolwenn <duflos.nol@laposte.net>
2023-06-06 14:44:32 +0200

    arbre

--- lecon_arbre.py
+++ lecon_arbre.py
@@ -9,5 +9,19 @@
 #Faire un arbre et trouver sa profondeur maximale et sa largeur maximale
 
 class arbre_binaire:
-    def __init__(self.a):
-        self.a = a
+    def __init__(self,racine = None, sag = None, sad = None):
+        self.racine = racine
+        self.sag = sag
+        self.sad = sad
+    def est_vide(self):
+        if self.racine is None:
+            return True
+        else:
+            return False
+
+a = arbre_binaire()
+a.racine = 1
+a.sag = arbre_binaire(2)
+a.sad = arbre_binaire(3)
+a.sag.sag = arbre_binaire(4)
+a.sad.sad = arbre_binaire()
