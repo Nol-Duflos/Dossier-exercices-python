7d5b409cf370dc3b0e8a8d69a51370ba7028e885
Nolwenn <duflos.nol@laposte.net>
2023-06-06 14:01:02 +0200

    push de début d'après midi, update des données

--- finance_app.py
+++ finance_app.py
@@ -8,3 +8,10 @@ balance = float(savings) * 1.05
 message = "savings in one year: " + str(balance)
 
 print(message)
+
+
+def taille(arbre_binaire):
+    if arbre_binaire.est_vide():
+        return 0
+    else:
+        return 1 + taille(arbre_binaire.sag) + taille(arbre_binaire.sad)
---
+++ lecon_arbre.py
@@ -0,0 +1,13 @@
+# class arbre_binaire:
+#     |--> constructeur
+#     |--> ajouter un enfant
+#     |--> verifier si on est une feuille
+#     |--> Trouver la profondeur de l'arbre 
+#     |--> parcours_profondeur 
+#     |--> parcours_largeur 
+
+#Faire un arbre et trouver sa profondeur maximale et sa largeur maximale
+
+class arbre_binaire:
+    def __init__(self.a):
+        self.a = a
