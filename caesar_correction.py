944aa8e811a471a0db4ce25b4ae234e057f6d01e
Nolwenn <duflos.nol@laposte.net>
2023-06-06 10:21:40 +0200

    Avancement de la progression - Viginere

--- caesar_correction.py
+++ caesar_correction.py
@@ -11,3 +11,15 @@ def chiffrer_caesar(message:str, cle) -> dict:
 def trouver_max_dico(dico:dict) -> str:
 
     return max(dico, key=lambda key:dico[key])
+
+def chiffrer_viginare(message:str, cle:tuple) -> str:
+    message_code = ""
+    index_cle = 0
+    for lettre in message:
+        message_code += chr(ord(lettre) + cle[index_cle])
+        index_cle = (index_cle + 1) % len(cle)
+    return message_code
+
+def dechiffrer_viginere(message_code:str, cle:tuple) -> str:
+    chiffrer_viginare(message_code, tuple([-elt for elt in cle]))
+
