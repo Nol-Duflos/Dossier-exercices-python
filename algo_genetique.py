3506f6afe6e79bd6cc2a0b886b142380ec9dfce0
Nolwenn <duflos.nol@laposte.net>
2023-06-08 13:40:53 +0200

    Ici c'est l'algo

--- algo_genetique.py
+++ algo_genetique.py
@@ -16,11 +16,11 @@ from string import ascii_letters
 
 #On déclare ensuite une fonction en ligne
 choix = lambda x: x[int(random() * len(x))]
-
+EXPECTED_STR = "Something something."
 
 #On déclare ensuite notre chaîne de caractères que le script devra deviner en un minimum de générations
 CHANCE_TO_MUTATE = 0.1
-GRADED_RETAIN_NONGRATED = 0.2
+GRADED_RETAIN_PERCENT = 0.2
 CHANCE_RETAIN_NONGRATED = 0.05
 #CHANCE_TO_MUTATE est un pourcentage qui determine la probabilité qu'un individu mute
 
@@ -28,4 +28,19 @@ CHANCE_RETAIN_NONGRATED = 0.05
 POPULATION_COUNT = 100
 GENERATION_COUNT_MAX = 100000
 
+#On déclare ensuite un certain nombre de constantes pour le reste du script.
+
+#Le nombre d’individus « hauts gradés » à retenir (GRADED_INDIVIDUAL_RETAIN_COUNT)
+GRADED_INDIVIDUAL_RETAIN_COUNT = int(POPULATION_COUNT * GRADED_RETAIN_PERCENT)
+#La taille de la chaîne de caractères à deviner (LENGTH_OF_EXPECTED_STR).
+LENGTH_OF_EXPECTED_STR = len(EXPECTED_STR)
+#La moitié de la taille de la chaîne de caractères à deviner (MIDDLE_LENGTH_OF_EXPECTED_STR).
+MIDDLE_LENGTH_OF_EXPECTED_STR = LENGTH_OF_EXPECTED_STR // 2
+#La liste des caractères autorisés (ALLOWED_CHARMAP).
+ALLOWED_CHARMAP = ascii_letters + ' !\'.'
+#Et pour finir, la valeur maximum du score qu’un individu peut obtenir (MAXIMUM_FITNESS).
+MAXIMUM_FITNESS = LENGTH_OF_EXPECTED_STR
+
+
 def algo_genetique():
+    
