ec9531e19a9b116e5a78940b6bcd2512abc6bc1c
Nolwenn <duflos.nol@laposte.net>
2023-06-08 09:51:40 +0200

    ajout des lists

--- sololearn_test.py
+++ sololearn_test.py
@@ -33,6 +33,24 @@
 # else:
 #     print("Normal temperature")
 
-myList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-listSum = sum(myList)
-print(f"Sum of list -> {listSum}")
+# myList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# listSum = sum(myList)
+# print(f"Sum of list -> {listSum}")
+
+# text = input("Entrez votre text : ")
+# print(text[2])
+
+# str = "Yay! I lernaed about lists"
+# print(str[0] + str[8])
+
+# nums = [5, 8, 2]
+# nums[1] = 42
+
+# print(nums)
+
+# nums=[10, 9, 8, 7, 6, 5]
+# nums[0] = nums[1]-5
+# if 4 in nums:
+#     print(nums[3])
+# else:
+#     print(nums[4])
